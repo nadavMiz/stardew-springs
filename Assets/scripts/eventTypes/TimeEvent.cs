e68ceb6c9a52ffba45ff86d1532e7977078b17ed
nadav mizrahi <34983648+nadavMiz@users.noreply.github.com>
2021-03-27 15:40:08 +0300

    example pull request

--- Assets/scripts/eventTypes/TimeEvent.cs
+++ Assets/scripts/eventTypes/TimeEvent.cs
@@ -10,4 +10,6 @@ public class TimeEvent
     public static string NOON_START = "TimeEvent_noonStart";
     public static string EVENING_START = "TimeEvent_eveningStart";
     public static string MIDNIGHT_START = "TimeEvent_midnightStart";
+
+    public static string MIDNIGHT_NADAV = "TimeEvent_nadav";
 }
